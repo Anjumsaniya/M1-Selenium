dfec7ef0b497db22c233b12de24236169e103611
SANIYA ANJUM <SANIYA ANJUM@LAPTOP-5U61PJ97>
2026-04-13 10:43:21 +0530

    there is some changes

---
+++ src/test/java/Demo/Practice2.java
@@ -0,0 +1,11 @@
+package Demo;
+
+public class Practice2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Changes to github");
+		System.out.println("There is a Changes");
+	}
+
+}
